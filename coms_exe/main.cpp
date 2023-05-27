62f57fa1fbbcf7cecbe18920f1a4118b8d636f0d
JozefDuda <j.duda56@outlook.com>
2023-05-27 19:14:38 +0000

    initial commit

---
+++ coms_exe/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <test_lib.hpp>
+#include <boost/asio.hpp>
+ 
+
+using boost::asio::ip::udp;
+// using ip::tcp;
+// using std::string;
+// using std::cout;
+// using std::endl;
+
+
+
+int main() {
+    std::cout << "Main test: " << COMS_VERSION << std::endl;
+    
+    boost::asio::io_service io_service;
+
+
+
+
+}   
+
+
+
+
+
+
---
+++ tools/test_lib/test_lib.hpp
@@ -0,0 +1,7 @@
+#ifndef _TEST_LIB_HPP_
+#define _TEST_LIB_HPP_
+
+
+#define COMS_VERSION 9U
+
+#endif/*_TEST_LIB_HPP_*/
