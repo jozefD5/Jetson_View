cbcb59ba8af3dfe56cf58d079986101d2e9904b7
JozefDuda <j.duda56@outlook.com>
2023-05-28 11:06:32 +0000

    added asio test to main

--- coms_exe/main.cpp
+++ coms_exe/main.cpp
@@ -2,19 +2,24 @@
 #include "test_lib.hpp"
 #include <boost/asio.hpp>
  
-
-//using boost::asio::ip::udp;
-// using ip::tcp;
-// using std::string;
-// using std::cout;
-// using std::endl;
+using namespace boost::asio::ip;
+using boost::asio::ip::udp;
+using std::string;
+using std::cout;
+using std::endl;
 
 
 
 int main() {
-    std::cout << "Main test: " << COMS_VERSION << std::endl;
+    std::cout << "Main test  \n\n";
     
-    boost::asio::io_service io_service;
+    boost::asio::io_service service;
+    tcp::endpoint endpoint(address::from_string("127.0.0.1"), 4000);
+    tcp::socket socket(service);
+
+    std::cout << "\n\n[Client] Connecting to server..." << std::endl;
+    socket.connect(endpoint);
+    std::cout << "\n\n[Client] Connection successful" << std::endl;
 
 
 
